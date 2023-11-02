ba4e04fc703b4df829cc06fa2c3ee7818b38bc76
Roman <romaholovai@gmail.com>
2023-11-02 19:16:44 +0100

    Initial Commit

---
+++ AdApp/AdAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdAppApp.swift
+//  AdApp
+//
+//  Created by Roman on 02/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct AdAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
